eb2367972709ebd01f5cecedc43dc8f0c94e3877
volodymyrshynkarenko <volodymyr.shynkarenko.digital@gmail.com>
2023-05-01 11:19:13 +0300

    upd

--- src/main/java/com/goit/Database.java
+++ src/main/java/com/goit/Database.java
@@ -9,8 +9,7 @@ public class Database {
     private Connection connection;
 
     private static final String DB_URL ="jdbc:h2:./test1";
-            //"http://192.168.31.143:8082?key=151bf6fa966d871bb5d53205682820d7e503d1343abe420a17c828c737bea05f";
-    //"jdbc:h2:~/data/parserpalce";
+
 
     private static final String DB_USER = "admin1";
     private static final String DB_PASSWORD = "12345";
